b93d2548f62c2182b34a71aafab8967b06f16165
Denis Lemos <denisrochalemos7@gmail.com>
2025-09-18 16:41:56 -0300

    add

---
+++ js/script.js
@@ -0,0 +1,38 @@
+function tlou(){
+let tlouimg = document.querySelector("img#imagem");
+let num = Math.trunc(Math.random()*10);
+
+if (num == 0) {
+	tlouimg.setAttribute ("src", "img/tlou.jpg");
+}
+else if (num == 1) {
+	tlouimg.setAttribute ("src", "img/tlou1.jpg");
+}
+else if (num == 2) {
+	tlouimg.setAttribute ("src", "img/tlou2.jpg");
+}
+else if (num == 3) {
+	tlouimg.setAttribute ("src", "img/tlou3.jpg");
+}
+else if (num == 4) {
+	tlouimg.setAttribute ("src", "img/tlou4.jpg");
+}
+else if (num == 5) {
+	tlouimg.setAttribute ("src", "img/tlou5.jpg");
+}
+else if (num == 6) {
+	tlouimg.setAttribute ("src", "img/tlou6.jpg");
+}
+else if (num == 7) {
+	tlouimg.setAttribute ("src", "img/tlou7.jpg");
+}
+else if (num == 8) {
+	tlouimg.setAttribute ("src", "img/tlou8.jpg");
+}
+else if (num == 9) {
+	tlouimg.setAttribute ("src", "img/tlou9.jpg");
+}
+else if (num == 10) {
+	tlouimg.setAttribute ("src", "img/download.jpg");
+}
+}
