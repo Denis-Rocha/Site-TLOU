baf44271bfb8be8280acc71bbaeb2e81b37f2246
Denis Lemos <denisrochalemos7@gmail.com>
2025-09-19 16:50:46 -0300

    add

--- js/script.js
+++ js/script.js
@@ -1,38 +1,17 @@
-function tlou(){
-let tlouimg = document.querySelector("img#imagem");
-let num = Math.trunc(Math.random()*10);
+document.addEventListener('DOMContentLoaded', function() {
+    const navLinks = document.querySelectorAll('header nav a');
+    const path = window.location.pathname.split('/').pop();
 
-if (num == 0) {
-	tlouimg.setAttribute ("src", "img/tlou.jpg");
-}
-else if (num == 1) {
-	tlouimg.setAttribute ("src", "img/tlou1.jpg");
-}
-else if (num == 2) {
-	tlouimg.setAttribute ("src", "img/tlou2.jpg");
-}
-else if (num == 3) {
-	tlouimg.setAttribute ("src", "img/tlou3.jpg");
-}
-else if (num == 4) {
-	tlouimg.setAttribute ("src", "img/tlou4.jpg");
-}
-else if (num == 5) {
-	tlouimg.setAttribute ("src", "img/tlou5.jpg");
-}
-else if (num == 6) {
-	tlouimg.setAttribute ("src", "img/tlou6.jpg");
-}
-else if (num == 7) {
-	tlouimg.setAttribute ("src", "img/tlou7.jpg");
-}
-else if (num == 8) {
-	tlouimg.setAttribute ("src", "img/tlou8.jpg");
-}
-else if (num == 9) {
-	tlouimg.setAttribute ("src", "img/tlou9.jpg");
-}
-else if (num == 10) {
-	tlouimg.setAttribute ("src", "img/download.jpg");
-}
-}
+    if (path) {
+        navLinks.forEach(link => {
+            if (link.getAttribute('href') === path) {
+                link.classList.add('active');
+            } else {
+                link.classList.remove('active');
+            }
+        });
+    } else {
+        // Assume que a página inicial é index.html
+        document.querySelector('header nav a[href="index.html"]').classList.add('active');
+    }
+});
